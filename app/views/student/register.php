f93dd12632c047e51c6123b2644843a2c58ca337
bouqrt <bouchraelqrt@gmail.com>
2026-01-15 15:25:16 +0100

    added the isncription form : signin html

---
+++ app/views/student/register.php
@@ -0,0 +1,8 @@
+<h2>Inscription</h2>
+
+<form method="post" action="/register">
+    <input type="text" name="name" placeholder="Nom" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Mot de passe" required>
+    <button type="submit">S'inscrire</button>
+</form>
