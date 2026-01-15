cf350758b068b0d29340572d0ed4cce7ba7eb6c4
bouqrt <bouchraelqrt@gmail.com>
2026-01-15 15:18:17 +0100

    added the student controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+class StudentController
+{
+    public function home()
+    {
+        echo "Page d'accueil";
+    }
+
+    public function login()
+    {
+        echo "Page de connexion";
+    }
+
+    public function register()
+    {
+        echo "Page d'inscription";
+    }
+
+    public function dashboard()
+    {
+        echo "Dashboard étudiant";
+    }
+}
