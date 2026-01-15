c7438b6d336394dd353413bfb195598353b7ed01
bouqrt <bouchraelqrt@gmail.com>
2026-01-15 10:09:09 +0100

    Ignore vendor directory

---
+++ app/core/Router.php
@@ -0,0 +1,48 @@
+<?php 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// namespace App\core;
+// class Router {
+
+//     private $route =[];
+
+//     public function route($path, $callback){
+//         $this->route[$path]= $callback;
+//     }
+
+//     public function dispatch(){
+//         $url =$_SERVER['REQUEST_URL'];
+        
+//         //echo "<pre>";
+//         //var_dump ($_SERVER['REQUEST_URL']);
+//         //echo"</pre>"
+
+//     };
+
+// }
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+use app\core \router
+    include _DIR__. '/../vendor/autoload.php';
+
+    $router = new router();
+    $router->route("/user"function($test)) -->
+
